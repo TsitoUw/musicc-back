2cfabbbbf548d89ffede863992d04fb4ce217e89
TsitoUw <tsitohainanirinarak2@gmail.com>
2023-03-02 23:16:07 +0300

    changed check

--- check.ts
+++ check.ts
@@ -3,20 +3,20 @@ import path from "path";
 
 export default function checkPublicDir(){
   try{
-    if(!existsSync(path.join(__dirname, '/public'))){
-      mkdirSync(path.join(__dirname, "/public"))
+    if(!existsSync(path.join(__dirname, 'public'))){
+      mkdirSync(path.join(__dirname, "public"))
     }
     
-    if(!existsSync(path.join(__dirname, '/public', "artwork"))){
-      mkdirSync(path.join(__dirname, "/public", "artwork"))
+    if(!existsSync(path.join(__dirname, 'public', "artwork"))){
+      mkdirSync(path.join(__dirname, "public", "artwork"))
     }
     
-    if(!existsSync(path.join(__dirname, '/public', "userpic"))){
-      mkdirSync(path.join(__dirname, "/public", "userpic"))
+    if(!existsSync(path.join(__dirname, 'public', "userpic"))){
+      mkdirSync(path.join(__dirname, "public", "userpic"))
     }
     
-    if(!existsSync(path.join(__dirname, '/public', "audio"))){
-      mkdirSync(path.join(__dirname, "/public", "audio"))
+    if(!existsSync(path.join(__dirname, 'public', "audio"))){
+      mkdirSync(path.join(__dirname, "public", "audio"))
     }
   }catch{
     console.log('fuck')
