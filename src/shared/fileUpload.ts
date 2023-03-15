8791a6a1ccdbef728f4a2ff9a6529dee93c41110
TsitoUw <tsitohainanirinarak2@gmail.com>
2023-03-15 16:41:33 +0300

    idk anymore :)

--- src/shared/fileUpload.ts
+++ src/shared/fileUpload.ts
@@ -1,18 +1,23 @@
 import { Request, Response } from "express";
 import { appendFileSync } from "fs";
 import path from "path";
+import imageUploader from "./imageUploader";
 
 export default function fileUpload(req:Request,res:Response,type:"artwork"|"profilepic"|"audio"){
   const fileName = req.params.filename;
-  const filePath = path.join(__dirname,"../../public",type,fileName);
-
-  res.status(206);
-  req.on("data", (chunk)=>{
-    appendFileSync(filePath,chunk);
-  })
-  req.on("error",(err)=>{
-    res.status(400).json({error:err});
-  })
-
-  res.json({message:"file uploaded"});
+  if(type === "audio"){
+    const filePath = path.join(__dirname,"../../public",type,fileName);
+    res.status(206);
+    req.on("data", (chunk)=>{
+      appendFileSync(filePath,chunk);
+    })
+    req.on("error",(err)=>{
+      res.status(400).json({error:err});
+    })
+  
+    res.json({message:"file uploaded"});
+  }else{
+    imageUploader(req,res,type,fileName);
+  }
+  
 }
